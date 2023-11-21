38d162704419b93105026beff27c4da4db470689
likecrong <112467598+likecrong@users.noreply.github.com>
2023-11-21 15:27:27 +0900

    Update appload.cy.js

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -48,47 +48,5 @@ describe('Test of login page', () => {
 		cy.wait(3000)
     })
 	
-	//2. 한국어 및 영어 전환
-	it('translate', () => {
-		cy.get('select').select('en') // 영어로 전환
-        cy.wait(6000)
-		cy.get('select').select('ko') // 한국어로 전환
-		cy.wait(6000)
-    })
 	
-	//3. login 실패 후, login 페이지로 이동
-	it('login Fail 1', () => {
-        cy.get('input[name="loginId"]').type('like.crong@gmail.com')
-		cy.get('input[name="password"]').type('nananono!02') //잘못된 비밀번호
-		cy.get('button[id="mui-3"]').click()
-
-        cy.wait(1000)
-    })
-	it('login Fail 2', () => {
-        cy.get('input[name="loginId"]').type('like.crong2@gmail.com') //잘못된 ID : 잘못된 이메일
-		cy.get('input[name="password"]').type('nananono!01') 
-		cy.get('button[id="mui-3"]').click()
-
-        cy.wait(1000)
-    })
-	it('login Fail 3', () => {
-        cy.get('input[name="loginId"]').type('like.crong') //잘못된 ID : 이메일 형식이 아님
-		cy.get('input[name="password"]').type('nananono!01')
-		cy.get('button[id="mui-3"]').click()
-
-        cy.wait(1000)
-    })
-	it('login Fail 4', () => {
-		cy.get('button[id="mui-3"]').click()
-        cy.wait(1000)
-    })
-	
-	//4. login 성공 후, account 설정페이지로 이동
-	it('login Success', () => {
-        cy.get('input[name="loginId"]').type('like.crong@gmail.com')
-		cy.get('input[name="password"]').type('nananono!01')
-		cy.get('button[id="mui-3"]').click()
-
-        cy.wait(1000)
-    })
 })
