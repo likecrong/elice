43242498f315be8f53c2b26346e26bc11cb9ddd0
likecrong <112467598+likecrong@users.noreply.github.com>
2023-11-21 16:51:12 +0900

    Update 01appLoad.cy.js

--- cypress/e2e/01appLoad.cy.js
+++ cypress/e2e/01appLoad.cy.js
@@ -2,20 +2,9 @@ describe('Test appload', () => {
 	beforeEach(() => {
       cy.visit('https://accounts.elice.io') 
     });
-	
-	//1. 앱 로드 확인
-	it('appLoad check', () => {
-		cy.get('input[name="loginId"]') // 이메일 필드 확인
-		cy.get('input[name="password"]') // 비밀번호 필드 확인
-		//cy.get('button[aria-label="비밀번호 보기"]') // 비밀번호 보기 버튼 확인
-		//cy.get('[class="MuiTypography-root MuiTypography-body2 MuiLink-root MuiLink-underlineHover css-q6237f"]').contains('비밀번호를 잊어버리셨나요?') // '비밀번호를 잊어버리셨나요?' 필드 확인
 		
-		cy.get('button[id="mui-3"]').contains('로그인')	 // '로그인' 버튼 필드 확인
-		
-		// '회원가입' 필드 확인
-		cy.get('[class="css-16k01br e1t19hrb0"]').contains('아직 계정이 없으신가요?')
-		cy.get('[class="css-16k01br e1t19hrb0"]').contains('회원가입')
-
-		cy.wait(3000)
+	//1. 상단 부 로고 확인
+	it('logo check', () => {
+		cy.get('[class="css-1xknvyq e1btkj5w1"]') 
     })
 })
