a189ad2458e94687c32a72e4460a2eacd2e2effa
likecrong <112467598+likecrong@users.noreply.github.com>
2023-11-21 17:00:14 +0900

    Update 01appLoad.cy.js

--- cypress/e2e/01appLoad.cy.js
+++ cypress/e2e/01appLoad.cy.js
@@ -5,6 +5,12 @@ describe('Test appload', () => {
 		
 	//1. 상단 부 로고 확인
 	it('logo check', () => {
-		cy.get('[class="css-1xknvyq e1btkj5w1"]') 
+		cy.get('[class="css-1xknvyq e1btkj5w1"]')
+    })
+	//2. 중반 부 확인
+	it('middle check', () => {
+		cy.get('input[name="loginId"]') // 이메일 필드 확인
+		cy.get('input[name="password"]') // 비밀번호 필드 확인
+		cy.get('[class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium  e15piai01 css-ydj31v"]') // 비밀번호 보기 버튼
     })
 })
