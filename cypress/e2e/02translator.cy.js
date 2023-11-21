bcea4f81f2807fdb7be832a31f0919126c93c204
likecrong <112467598+likecrong@users.noreply.github.com>
2023-11-21 18:35:01 +0900

    Update 02translator.cy.js

--- cypress/e2e/02translator.cy.js
+++ cypress/e2e/02translator.cy.js
@@ -3,12 +3,12 @@ describe('Test translator', () => {
       cy.visit('https://accounts.elice.io') 
     });
 	
-	//2. 한국어 및 영어 전환
+	//한국어 및 영어 전환
 	it('translate', () => {
 		cy.get('select').select('en') // 영어로 전환
-        cy.wait(6000)
+        cy.wait(2000)
 		cy.get('select').select('ko') // 한국어로 전환
-		cy.wait(6000)
+		cy.wait(2000)
     })
 	
 })
