73452482e3f003f21a19f34f4cdbc4585ca97ca1
likecrong <112467598+likecrong@users.noreply.github.com>
2023-11-21 18:36:21 +0900

    Update 03loginFail.cy.js

--- cypress/e2e/03loginFail.cy.js
+++ cypress/e2e/03loginFail.cy.js
@@ -3,28 +3,36 @@ describe('Test loginFail', () => {
       cy.visit('https://accounts.elice.io') 
     });
 	
-	//3. login 실패 후, login 페이지로 이동
+	//login 실패 후, login 페이지로 이동
+	
+	// 아이디와 매칭되지 않는 비밀번호를 입력할 때
 	it('login Fail 1', () => {
         cy.get('input[name="loginId"]').type('like.crong@gmail.com')
-		cy.get('input[name="password"]').type('nananono!02') //잘못된 비밀번호
+		cy.get('input[name="password"]').type('nananono!02')
 		cy.get('button[id="mui-3"]').click()
 
         cy.wait(1000)
     })
+	
+	// 아이디 입력시, 존재하지 않는 이메일을 입력할 때
 	it('login Fail 2', () => {
-        cy.get('input[name="loginId"]').type('like.crong2@gmail.com') //잘못된 ID : 잘못된 이메일
+        cy.get('input[name="loginId"]').type('like.crong2@gmail.com')
 		cy.get('input[name="password"]').type('nananono!01') 
 		cy.get('button[id="mui-3"]').click()
 
         cy.wait(1000)
     })
+	
+	// 아이디 입력시, 이메일 형식이 아닐 때
 	it('login Fail 3', () => {
-        cy.get('input[name="loginId"]').type('like.crong') //잘못된 ID : 이메일 형식이 아님
+        cy.get('input[name="loginId"]').type('like.crong')
 		cy.get('input[name="password"]').type('nananono!01')
 		cy.get('button[id="mui-3"]').click()
 
         cy.wait(1000)
     })
+	
+	// 아이디와 비밀번호를 입력하지 않고 로그인 버튼을 누를 때
 	it('login Fail 4', () => {
 		cy.get('button[id="mui-3"]').click()
         cy.wait(1000)
