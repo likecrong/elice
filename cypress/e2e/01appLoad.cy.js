347e7392c5c905985e771a9d3e5d5a6f7582fe57
likecrong <112467598+likecrong@users.noreply.github.com>
2023-11-21 17:11:47 +0900

    Update 01appLoad.cy.js

--- cypress/e2e/01appLoad.cy.js
+++ cypress/e2e/01appLoad.cy.js
@@ -12,6 +12,8 @@ describe('Test appload', () => {
 		cy.get('input[name="loginId"]') // 이메일 필드 확인
 		cy.get('input[name="password"]') // 비밀번호 필드 확인
 		cy.get('[class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium  e15piai01 css-ydj31v"]') // 비밀번호 보기 버튼
-		cy.get('[class="MuiTypography-root MuiTypography-body2 MuiLink-root MuiLink-underlineHover css-q6237f"]').should('have.attr', 'href') // '비밀번호를 잊어버리셨나요?' 필드 확인
+		cy.get('[class="MuiTypography-root MuiTypography-body2 MuiLink-root MuiLink-underlineHover css-q6237f"]').should('have.attr', 'href') // '비밀번호를 잊어버리셨나요?' 링크 확인
+		
+		cy.get('button[id="mui-3"]').click()	 // '로그인' 버튼 확인
     })
 })
